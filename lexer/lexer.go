45fca20cefa2d211814c3aedb6449a6cb646d5c3
Allen Gao <allen.gao@datadoghq.com>
2023-04-09 00:57:44 -0400

    Add basic lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,119 @@
+package lexer
+
+import (
+	"monkey.interpreter/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // Current position.
+	readPosition int  // Current reading position to peek.
+	ch           byte // Current char being checked.
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// readChar reads in the current character and advances the position.
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // ASCII for 'NUL' character.
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// NextToken reads in the next token.
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// We return early, because readIdentifier already advances current position
+			// to next non-letter.
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+// TODO: can combine readIdentifier and readNumber into one, pass in function
+// to generalize behaviour.
+
+// readIdentifier reads an identifier and advances the lexer's position until
+// a non-letter-character.
+func (l *Lexer) readIdentifier() string {
+	initPosition := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[initPosition:l.position]
+}
+
+// TODO: FEAT: support different number notations.
+
+func (l *Lexer) readNumber() string {
+	initPosition := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[initPosition:l.position]
+}
+
+// skipWhitespace skips whitespace. Also called 'eatWhitespace' or 'consumeWhitespace'.
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// isLetter returns whether a character is a 'letter'.
+// This is where we would sneak in what we can include in var names.
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
